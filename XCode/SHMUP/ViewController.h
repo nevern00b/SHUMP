71ce30a916eafcf82e411f0e10eb66a7f74c13f4
Sean Lilley <lilleyse@gmail.com>
2014-05-01 14:40:05 -0400

    added XCode project, with touch controls working

---
+++ XCode/SHMUP/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SHMUP
+//
+//  Created by Philadelphia Game Lab on 4/28/14.
+//  Copyright (c) 2014 Philadelphia Game Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
