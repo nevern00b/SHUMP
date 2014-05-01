fdd9189594e2deab4454a90586b84f17f30b95c8
wuhao1117 <wuhao1@seas.upenn.edu>
2014-05-01 10:31:11 -0400

    update minion source file

---
+++ src/Game/Minion.cpp
@@ -0,0 +1,124 @@
+#include "Minion.h"
+
+//#include <Box2D/Box2D.h>
+#include "Rendering/Material.h"
+#include "Globals.h"
+#include "Physics/PhysicsComponent.h"
+#include "Rendering/RenderComponent.h"
+#include "Physics/PhysicsManager.h"
+#include "DataManager.h"
+#include "UIManager.h"
+#include "ShmupGame.h"
+#include "ShootComponent.h"
+#include "Utils.h"
+#include "Bullet.h"
+#include "Item.h"
+#include "Player.h"
+
+Minion::Minion(b2Vec2 relativePos) : Entity(0), m_lives(1)
+{
+	Material* material = new Material();
+	material->m_noiseStrength = 0.1f;
+	Mesh* mesh = Globals::m_dataManager->getMesh("sphere");
+
+	b2PolygonShape shape; 
+	shape.SetAsBox(0.5f, 0.5f);
+
+	PhysicsData physicsData(shape);
+	physicsData.m_groupIndex = ShmupGame::PLAYER_GROUP;
+	PhysicsComponent* physics = new PhysicsComponent(this, physicsData);
+	RenderComponent* render = new RenderComponent(this, mesh, material);
+	m_shootComponent = new ShootComponent(this, Globals::m_shmupGame->m_redBulletPool);
+
+	m_shootTimer = new Timer(0.2f);
+	m_player = Globals::m_shmupGame->getPlayer();
+	m_relativePos = relativePos;
+}
+
+Minion::~Minion()
+{
+
+}
+
+bool Minion::update()
+{
+	if (!Entity::update()) return false;
+
+	b2Vec2 pos = m_physics->m_body->GetPosition();
+	b2Vec2 targetPos = m_player->GetPosition() + m_relativePos;
+
+	float speed = 10.0f;
+	float vx = targetPos.x - pos.x;
+	float vy = targetPos.y - pos.y;
+
+	// Auto shoot
+	if (m_shootTimer->checkInterval())
+	{
+		shoot();
+	}
+
+	m_physics->applyVelocity(vx, vy);
+
+	return true;
+}
+
+void Minion::onCollide(EventObject* collider)
+{
+	Bullet* bullet = dynamic_cast<Bullet*>(collider);
+	//ImmunityItem* immunityItem = dynamic_cast<ImmunityItem*>(collider);
+	LifeItem* lifeItem = dynamic_cast<LifeItem*>(collider);
+
+	if (bullet != 0)
+	{
+		bullet->destroy();
+
+		if (bullet->m_color != Globals::m_stateMachine->getIState())
+		{
+			m_lives--;
+			if (m_lives == 0)
+			{
+				destroy();
+			}
+		}
+	}
+	/*else if (immunityItem != 0)
+	{
+		COLOR color = immunityItem->m_color;
+		Globals::m_stateMachine->changeIState(color);
+		Globals::m_stateMachine->changeBState(color);
+		immunityItem->destroy();
+	}*/
+	else if (lifeItem != 0)
+	{
+		m_lives++;
+		lifeItem->destroy();
+	}
+
+}
+
+
+void Minion::shoot()
+{
+	int bulletState = Globals::m_stateMachine->getBState();
+	if (bulletState == COLOR::RED)
+	{
+		m_shootComponent->m_bulletPool = Globals::m_shmupGame->m_redBulletPool;
+	}
+	else if (bulletState == COLOR::BLUE)
+	{
+		m_shootComponent->m_bulletPool = Globals::m_shmupGame->m_blueBulletPool;
+	}
+	else if (bulletState == COLOR::GREEN)
+	{
+		m_shootComponent->m_bulletPool = Globals::m_shmupGame->m_greenBulletPool;
+	}
+	else if (bulletState == COLOR::YELLOW)
+	{
+		m_shootComponent->m_bulletPool = Globals::m_shmupGame->m_yellowBulletPool;
+	}
+
+	b2Vec2 pos = m_physics->m_body->GetPosition();
+	float vx = 0.0f;
+	float vy = 15.0f;
+	m_shootComponent->shoot(pos.x, pos.y, vx, vy);
+}
